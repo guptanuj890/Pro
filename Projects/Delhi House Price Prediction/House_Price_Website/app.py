e6d86954cfa95a3f718b25d72de822fb110c036f
Anuj Gupta <guptanuj890@gmail.com>
2023-09-19 23:02:16 +0530

    modified:   Projects/Delhi House Price Prediction/House_Price_Website/app.py 	new file:   Projects/Delhi House Price Prediction/House_Price_Website/rfr.pkl 	renamed:    Projects/Delhi House Price Prediction/House_Price_Website/static/style.css -> Projects/Delhi House Price Prediction/House_Price_Website/static/styles.css 	modified:   Projects/Delhi House Price Prediction/House_Price_Website/templates/index.html 	new file:   Projects/Delhi House Price Prediction/House_Price_Website/templates/result.html

--- Projects/Delhi House Price Prediction/House_Price_Website/app.py
+++ Projects/Delhi House Price Prediction/House_Price_Website/app.py
@@ -8,8 +8,27 @@ app = Flask(__name__)
 @app.route('/')
 
 @app.route("/House")
-def cancer():
+def home():
     return render_template("index.html")
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==10):
+        loaded_model = joblib.load(r'C:\Users\guptanuj890\OneDrive\Desktop\pro\Projects\Delhi House Price Prediction\House_Price_Website\rfr.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        
+        if(len(to_predict_list)==10):
+            result = ValuePredictor(to_predict_list,10)
+    
+    return(render_template("result.html", prediction_text=result))       
 
 if __name__ == '__main__':
 
